0ee94da7d90454cd6865ce4c31bb36432de3ec1e
yogesh.paisode <yogesh.paisode@gmail.com>
2015-01-02 10:59:15 +0000

    hello

--- JD3PT3/src/com/JD/INIT_Data/Init_Data.java
+++ JD3PT3/src/com/JD/INIT_Data/Init_Data.java
@@ -4,6 +4,7 @@
  */
 package com.JD.INIT_Data;
 
+import java.io.File;
 import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.Session;
@@ -12,33 +13,36 @@ import org.hibernate.Session;
  *
  * @author Yogesh
  */
-public class Init_Data implements Runnable{
+public class Init_Data implements Runnable {
 
-    public Init_Data() {    
-        new Thread(this, "INIT DATA").start();    
-    }  
-        
+    public Init_Data() {
+        new Thread(this, "INIT DATA").start();
+    }
+
     @Override
     public void run() {
-        
-        Session session=com.JD.StaticData.Static_DATA.init_SessionFactory.openSession();
-        
-        Query q=session.createQuery("from com.JD.InitData.Hibernate.config.Initdata");
-        
-        List list=q.list();
-        
+
+        Session session = com.JD.StaticData.Static_DATA.init_SessionFactory.openSession();
+
+        Query q = session.createQuery("from com.JD.InitData.Hibernate.config.Initdata");
+
+        List list = q.list();
+
         for (Object object : list) {
-            
-            com.JD.InitData.Hibernate.config.Initdata id=(com.JD.InitData.Hibernate.config.Initdata)object;
-            com.JD.StaticData.Static_DATA.location=id.getLocation();
-            com.JD.StaticData.Static_DATA.prSrNo=id.getPrSrNo();
-            com.JD.StaticData.Static_DATA.srNo=id.getSrNo();
-            com.JD.StaticData.Static_DATA.registrationNumber=id.getRegistrationNumber();  
-            com.JD.StaticData.Static_DATA.selfPartyname=id.getRawField1();
-            
+
+            com.JD.InitData.Hibernate.config.Initdata id = (com.JD.InitData.Hibernate.config.Initdata) object;
+            com.JD.StaticData.Static_DATA.location = id.getLocation();
+            com.JD.StaticData.Static_DATA.prSrNo = id.getPrSrNo();
+            com.JD.StaticData.Static_DATA.srNo = id.getSrNo();
+            com.JD.StaticData.Static_DATA.registrationNumber = id.getRegistrationNumber();
+            com.JD.StaticData.Static_DATA.selfPartyname = id.getRawField1();
+            File file = new File("temp");
+            file.delete();
+            file = new File("temp");
+            file.mkdir();
+
         }
-        
-        
+
+
     }
-    
 }
