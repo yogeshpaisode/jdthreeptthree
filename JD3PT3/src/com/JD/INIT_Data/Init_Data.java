74b57f6d72aed10e9082430c0a72e98db0301833
yogesh.paisode <yogesh.paisode@gmail.com>
2015-01-08 08:47:01 +0000

    hello

--- JD3PT3/src/com/JD/INIT_Data/Init_Data.java
+++ JD3PT3/src/com/JD/INIT_Data/Init_Data.java
@@ -6,6 +6,7 @@ package com.JD.INIT_Data;
 
 import java.io.File;
 import java.util.List;
+import javax.swing.JOptionPane;
 import org.hibernate.Query;
 import org.hibernate.Session;
 
@@ -21,13 +22,9 @@ public class Init_Data implements Runnable {
 
     @Override
     public void run() {
-
         Session session = com.JD.StaticData.Static_DATA.init_SessionFactory.openSession();
-
         Query q = session.createQuery("from com.JD.InitData.Hibernate.config.Initdata");
-
         List list = q.list();
-
         for (Object object : list) {
             new DeletTemp();
             com.JD.InitData.Hibernate.config.Initdata id = (com.JD.InitData.Hibernate.config.Initdata) object;
@@ -36,6 +33,10 @@ public class Init_Data implements Runnable {
             com.JD.StaticData.Static_DATA.srNo = id.getSrNo();
             com.JD.StaticData.Static_DATA.registrationNumber = id.getRegistrationNumber();
             com.JD.StaticData.Static_DATA.selfPartyname = id.getRawField1();
-        }
+        }        
+        File file = new File("temp");        
+	if (!file.exists()) {            
+	    file.mkdir();
+	}
     }
 }
